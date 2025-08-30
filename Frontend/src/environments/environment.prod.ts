ba88d73bc1522a3092e870ca76ab1568f5698671
Kgaogelo072 <Fortunatekgaogelo@gmail.com>
2025-08-30 02:48:28 +0200

    Fresh frontend deployment with correct API URL

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://issuetracker-api.azurewebsites.net/api',
-  version: '2.0.0' // Force cache busting
+  apiUrl: 'https://issuetracker-api.azurewebsites.net/api'
 };
