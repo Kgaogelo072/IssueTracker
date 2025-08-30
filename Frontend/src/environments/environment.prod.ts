1ed02eb9e286a155d3f5f0669a36415c807266fd
Kgaogelo072 <Fortunatekgaogelo@gmail.com>
2025-08-30 02:18:15 +0200

    Add cache busting version parameter to force fresh deployment

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://issuetracker-api.azurewebsites.net/api'
+  apiUrl: 'https://issuetracker-api.azurewebsites.net/api',
+  version: '2.0.0' // Force cache busting
 };
